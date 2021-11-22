7544f46416c20768db7708cb79ec42611d8981c8
Shivam Upadhyay <71204756+Shivamup99@users.noreply.github.com>
2021-11-22 23:31:11 +0530

    Update Main.jsx

--- src/data/Main.jsx
+++ src/data/Main.jsx
@@ -13,9 +13,9 @@ function Main() {
     <div className="collapse navbar-collapse" id="navbarResponsive">
       <ul className="navbar-nav">
         <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#about">About</a></li>
-        <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#experience">Experience & Project</a></li>
-        <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#education">Education</a></li>
-        <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#skills">Skills</a></li>
+      //  <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#experience">Experience & Project</a></li>
+      //  <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#education">Education</a></li>
+      //  <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#skills">Skills</a></li>
         <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#interests">Interests</a></li>
         <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#awards">Awards</a></li>
       </ul>
